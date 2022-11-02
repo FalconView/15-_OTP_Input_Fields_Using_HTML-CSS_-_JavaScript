9290969098584adf6eb4584bbd92f6a1ead4457a
Feroz Zargar <Ferozzargar@hotmail.com>
2022-11-02 18:20:53 -0400

    script.js was created in js folder and hard coded

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const carouselContainer = document.querySelector(".carouselContainer");
+const slides = document.querySelectorAll('.slides');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+let counter = 1;
+const size = slide[0].clientWidth;
+carouselContainer.style.transform = 'translateX(' + (-size * counter ) + 'px'; next.addEventListener('click', ()=>{
+    if (counter>=slides.length)
+    return;
+    carouselContainer.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    carouselContainer.style.transform = 'translateX(' + * counter ) + 'px';
+});
